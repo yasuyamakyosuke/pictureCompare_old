f042265d31143b0d60b17a10bb6fa0bdbe818846
泰山恭輔 <>
2023-05-09 16:38:36 +0900

    プロジェクト作成

---
+++ pictureCompare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pictureCompare
+//
+//  Created by 泰山恭輔 on 2023/05/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pictureCompareUITests/pictureCompareUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  pictureCompareUITestsLaunchTests.swift
+//  pictureCompareUITests
+//
+//  Created by 泰山恭輔 on 2023/05/09.
+//
+
+import XCTest
+
+final class pictureCompareUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
